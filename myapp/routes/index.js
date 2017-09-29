e6192a1861efcb80f420ffb2b0d87bf0833d565a
James Schulman <jaschul@bu.edu>
2017-09-29 12:07:06 -0400

    Embedded pdf resume

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/resume', function (req, res, next) {
+  return res.render('resume')
+})
+
 module.exports = router;
